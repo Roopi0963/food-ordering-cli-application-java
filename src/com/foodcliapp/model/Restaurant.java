26c3da3a1f7c71de20025c2e24c88d124fb48273
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-01-06 23:55:00 +0530

    feat: Add model class Restaurant

---
+++ src/com/foodcliapp/model/Restaurant.java
@@ -0,0 +1,10 @@
+package com.foodcliapp.model;
+
+import java.util.List;
+
+public class Restaurant {
+    private String id;
+    private String name;
+    private String address;
+    private List<Dish> menu;
+}
