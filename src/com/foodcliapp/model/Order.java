8a2c7935ca364a17c60ebfb7bf43dc8ed11dbaf0
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-01-06 18:42:43 +0530

    feat: Add model classes Customer,Dish,Order,Restaurant

---
+++ src/com/foodcliapp/model/Order.java
@@ -0,0 +1,10 @@
+package com.foodcliapp.model;
+
+import java.util.List;
+
+public class Order {
+    private Dish id;
+    private Customer customer;
+    private Restaurant restaurant;
+    private List<Dish> dishes;
+}
