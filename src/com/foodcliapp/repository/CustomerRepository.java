ec3bbab128819737928c0b07da5b0b39735c5f28
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-01-09 19:18:02 +0530

    feat: Add CustomerRepository to get customer data

---
+++ src/com/foodcliapp/repository/CustomerRepository.java
@@ -0,0 +1,21 @@
+package com.foodcliapp.repository;
+
+import com.foodcliapp.model.Customer;
+import com.foodcliapp.util.CsvReader;
+
+import java.util.List;
+
+public class CustomerRepository {
+    private List<Customer> customersList;
+
+
+    public CustomerRepository(){
+        CsvReader csvReader = new CsvReader();
+//        csvReader.readCustomerFromCsv();
+        this.customersList=csvReader.readCustomerFromCsv();
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customersList;
+    }
+}
