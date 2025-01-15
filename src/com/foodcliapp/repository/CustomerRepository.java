7174c2d2164ef000fe76d1cde653452aacddc304
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-01-15 19:46:56 +0530

    feat: Add CustomerRepository to read data and save new data

--- src/com/foodcliapp/repository/CustomerRepository.java
+++ src/com/foodcliapp/repository/CustomerRepository.java
@@ -4,6 +4,7 @@ import com.foodcliapp.model.Customer;
 import com.foodcliapp.util.CsvReader;
 
 import java.util.List;
+import java.util.Optional;
 
 public class CustomerRepository {
     private List<Customer> customersList;
@@ -18,5 +19,18 @@ public class CustomerRepository {
     public List<Customer> getCustomersList() {
 
         return customersList;
+    }
+
+    public Customer save(Customer customer){
+        this.customersList.add(customer);
+        return customer;
+    }
+
+
+    public Optional<Customer> findCustomerById(String id){
+        return this.customersList.stream().filter(customer -> customer.getId().equals(id)).findAny();
+    }
+    public Optional<Customer> findCustomerByEmail(String email){
+        return this.customersList.stream().filter(customer -> customer.getEmail().equals(email)).findAny();
     }
 }
