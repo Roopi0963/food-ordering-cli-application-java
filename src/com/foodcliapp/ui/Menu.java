721bc97c1b6c1b9bd044455458fe38f7c6a8dd86
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-01-25 10:16:01 +0530

    feat: Implemented RegisteredMenu method

--- src/com/foodcliapp/ui/Menu.java
+++ src/com/foodcliapp/ui/Menu.java
@@ -33,7 +33,7 @@ public class Menu {
             int input= scanner.nextInt();
             switch (input){
                 case 1:
-//                    displayRegisterMenu();
+                    displayRegisterMenu();
                     break;
                 case 2:
                     System.out.println("Thank you..............! ");
@@ -44,48 +44,48 @@ public class Menu {
         }
     }
 
-//    public void displayRegisterMenu(){
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Please register using following details");
-//        System.out.println("Please enter Id");
-//        String id = scanner.nextLine();
-//        System.out.println("Enter Name");
-//        String name = scanner.nextLine();
-//        System.out.println("Enter mail");
-//        String email = scanner.nextLine();
-//        System.out.println("enter password");
-//        String password = scanner.nextLine();
-//
-//        Customer customer = new Customer();
-//        customer.setId(id);
-//        customer.setName(name);
-//        customer.setEmail(email);
-//        customer.setPassword(password);
-//
-//        CustomerController customerController = Factory.getCustomerController();
-//
-//        try{
-//                Customer savedCustomer = customerController.save(customer);
-//                if(savedCustomer != null){
-//                    System.out.println("Customer registration is successful ");
-//                    System.out.println("Details : ");
-//                    System.out.println("Id : "+ customer.getId());
-//                    System.out.println("Name : "+customer.getName());
-//                    System.out.println("Email : "+customer.getEmail());
-//                    System.out.println("Password : "+customer.getPassword());
-//
-//
-//                }
-//                else{
-//                    System.out.println("Some internal error occurred, Please try again ");
-//                    displayRegisterMenu();
-//                }
-//
-//        }catch (CustomerExistException e){
-//            System.out.println(e.getMessage());
-//            System.out.println("Customer already exist, Please login using main menu");
-//            displayMainMenu();
-//
-//        }
+    public void displayRegisterMenu(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please register using following details");
+        System.out.println("Please enter Id");
+        String id = scanner.nextLine();
+        System.out.println("Enter Name");
+        String name = scanner.nextLine();
+        System.out.println("Enter mail");
+        String email = scanner.nextLine();
+        System.out.println("enter password");
+        String password = scanner.nextLine();
+
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setName(name);
+        customer.setEmail(email);
+        customer.setPassword(password);
+
+        CustomerController customerController = Factory.getCustomerController();
+
+        try{
+                Customer savedCustomer = customerController.save(customer);
+                if(savedCustomer != null){
+                    System.out.println("Customer registration is successful ");
+                    System.out.println("Details : ");
+                    System.out.println("Id : "+ customer.getId());
+                    System.out.println("Name : "+customer.getName());
+                    System.out.println("Email : "+customer.getEmail());
+                    System.out.println("Password : "+customer.getPassword());
+
+
+                }
+                else{
+                    System.out.println("Some internal error occurred, Please try again ");
+                    displayRegisterMenu();
+                }
+
+        }catch (CustomerExistException e){
+            System.out.println(e.getMessage());
+            System.out.println("Customer already exist, Please login using main menu");
+            displayMainMenu();
+
+        }
     }
 }
