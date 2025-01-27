8d852ec0a8aebdeff5a397d8abfa8279a280f048
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-01-27 21:13:20 +0530

    feat: Implemented a method to get customer by id

--- src/com/foodcliapp/service/CustomerService.java
+++ src/com/foodcliapp/service/CustomerService.java
@@ -1,10 +1,12 @@
 package com.foodcliapp.service;
 
 import com.foodcliapp.exceptions.CustomerExistException;
+import com.foodcliapp.exceptions.CustomerNotFoundException;
 import com.foodcliapp.model.Customer;
 
 public interface CustomerService {
     public Customer save(Customer customer) throws CustomerExistException;
+    public Customer getCustomerById(String id) throws CustomerNotFoundException;
 
 
 }
