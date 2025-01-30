602fbe805d015afe232732900955ad851b946d5c
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-01-31 01:42:08 +0530

    feat: Implemented Customer CRUD operations

--- src/com/foodcliapp/controller/CustomerController.java
+++ src/com/foodcliapp/controller/CustomerController.java
@@ -1,8 +1,11 @@
 package com.foodcliapp.controller;
 
 import com.foodcliapp.exceptions.CustomerExistException;
+import com.foodcliapp.exceptions.CustomerNotFoundException;
 import com.foodcliapp.model.Customer;
 import com.foodcliapp.service.CustomerServiceImpl;
+
+import java.util.List;
 
 public class CustomerController {
     private CustomerServiceImpl customerService;
@@ -13,5 +16,30 @@ public class CustomerController {
 
     public Customer save(Customer customer) throws CustomerExistException {
         return this.customerService.save(customer);
+    }
+
+    public Customer validateCustomerLogin(String email,String password) throws CustomerNotFoundException{
+        Customer customer = this.customerService.validateCustomerLogin(email,password);
+        if(customer != null)
+            this.customerService.setCurrentLoggedInCustomer(customer);
+        return customer;
+    }
+
+    public Customer currentLoggedInCustomer(){
+        return this.customerService.getCurrentLoggedInCustomer();
+    }
+
+    public Customer getCustomerById(String id) throws CustomerNotFoundException{
+        return this.customerService.getCustomerById(id);
+
+    }
+
+    public List<Customer> getCustomersList(){
+        return this.customerService.getAllCustomers();
+    }
+
+    public Customer updateCustomer(Customer customer) throws  CustomerNotFoundException{
+        return this.customerService.update(String.valueOf(customer));
+
     }
 }
