ff0fab9edad87704c5c01b85899f46882cc9d454
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-01-30 10:26:32 +0530

    feat: Implemented all Restaurant class to create objects

--- src/com/foodcliapp/ui/Factory.java
+++ src/com/foodcliapp/ui/Factory.java
@@ -1,8 +1,11 @@
 package com.foodcliapp.ui;
 
 import com.foodcliapp.controller.CustomerController;
+import com.foodcliapp.controller.RestaurantController;
 import com.foodcliapp.repository.CustomerRepository;
+import com.foodcliapp.repository.RestaurantRepository;
 import com.foodcliapp.service.CustomerServiceImpl;
+import com.foodcliapp.service.RestaurantServiceImpl;
 
 public class Factory {
 
@@ -18,5 +21,17 @@ public class Factory {
     public static CustomerController getCustomerController(){
 
         return new CustomerController(getCustomerService());
+    }
+
+
+    public static RestaurantRepository getRestaurantRepository(){
+        return new RestaurantRepository();
+    }
+    public static RestaurantServiceImpl getRestaurantService(){
+        return new RestaurantServiceImpl(getRestaurantRepository());
+    }
+
+    public static RestaurantController getrestaurantController(){
+        return new RestaurantController(getRestaurantService());
     }
 }
