101807f1ae2b81e6f91f0b4b1f9dfed2eb18fe74
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-01-30 23:53:40 +0530

    feat: Implemented menu details

---
+++ src/com/foodcliapp/ui/RestaurantMenu.java
@@ -0,0 +1,13 @@
+package com.foodcliapp.ui;
+
+import java.util.Scanner;
+
+public class RestaurantMenu {
+    public void displayRestaurantMenu(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Restaurant  details :-> ");
+
+
+
+    }
+}
