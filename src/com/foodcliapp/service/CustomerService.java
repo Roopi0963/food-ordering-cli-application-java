9b86a7ca668012118a589eccfd280b3fbbba67e4
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-01-31 01:09:55 +0530

    feat: Implemented Customer CRUD operations

--- src/com/foodcliapp/service/CustomerService.java
+++ src/com/foodcliapp/service/CustomerService.java
@@ -4,10 +4,26 @@ import com.foodcliapp.exceptions.CustomerExistException;
 import com.foodcliapp.exceptions.CustomerNotFoundException;
 import com.foodcliapp.model.Customer;
 
+import java.util.List;
+
 public interface CustomerService {
     public Customer save(Customer customer) throws CustomerExistException;
+
     public Customer getCustomerById(String id) throws CustomerNotFoundException;
-    
+
+    public List<Customer> getAllCustomers();
+
+    public Customer update(String id) throws CustomerNotFoundException;
+
+    public void delete(String id) throws CustomerNotFoundException;
+
+    public Customer validateCustomerLogin(String email,String password) throws CustomerNotFoundException;
+
+    public void setCurrentLoggedInCustomer(Customer customer);
+
+    public Customer getCurrentLoggedInCustomer();
+
+
 
 
 }
