21105efb1a1a4d063d3e45db04fff75188270583
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-02-01 23:28:02 +0530

    feat: Implemented Menu class method to display menu header

--- src/com/foodcliapp/ui/Menu.java
+++ src/com/foodcliapp/ui/Menu.java
@@ -18,12 +18,12 @@ public class Menu {
 
     }
 
-//    public void displayMainMenu(String menuHeader){
-//        printDashLines();
-//        String spaces = new String(new char[70]).replace('\0',' ');
-//        System.out.printf("%-70s %-10s %-70s \n", spaces, menuHeader, spaces);
-//        printDashLines();
-//    }
+    public void displayMainMenu(String menuHeader){
+        printDashLines();
+        String spaces = new String(new char[70]).replace('\0',' ');
+        System.out.printf("%-70s %-10s %-70s \n", spaces, menuHeader, spaces);
+        printDashLines();
+    }
 //    public void printDashLines(){
 //        String dashLines = new String(new char[150]).replace('\0','-');
 //        System.out.println(dashLines);
