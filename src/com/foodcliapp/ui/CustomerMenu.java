fe3cb5ea1f402a680b512209d76820bf81f4e3d6
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-02-02 10:20:46 +0530

    feat: Implemented CRUD operations

--- src/com/foodcliapp/ui/CustomerMenu.java
+++ src/com/foodcliapp/ui/CustomerMenu.java
@@ -8,7 +8,44 @@ import com.foodcliapp.model.Customer;
 import java.util.Scanner;
 
 public class CustomerMenu {
-    public static void displayRegisterMenu(){
+    public void displayCustomerMenu(){
+        try{
+            Scanner scanner = new Scanner(System.in);
+            displayMenuHeader("WELCOME CUSTOMER SECTION");
+            System.out.println();
+            System.out.println("Please select the option !");
+            System.out.println("1. Register (New Customer)");
+            System.out.println("2. Login (Existing Customer)");
+            System.out.println("3. Search Customer");
+            System.out.println("4. Display all Customers");
+            System.out.println("5. Update Customer");
+            System.out.println("6. Delete Customer");
+            System.out.println("7. Exit");
+            int input= scanner.nextInt();
+            switch(input){
+                case 1 -> customerRegisterForm();
+                case 2 -> customerLoginForm();
+//                case 3 -> customerSearchForm();
+//                case 4 -> displayAllCustomers();
+                case 5 -> customerUpdateForm();
+//                case 6 -> customerDeleteForm();
+                case 7 -> {
+                    System.out.println("Thank You.See you again....");
+                    displayCustomerMenu();
+                }
+                default -> System.out.println("Invalid input. Please enter valid input from(1-7) ");
+
+            }
+        } catch (Exception e) {
+            System.out.println("Some internal error occurred.Please try again.....! ");
+            displayCustomerMenu();
+        }
+
+    }
+
+
+
+    public static void customerRegisterForm(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please register using following details");
         System.out.println("Please enter Id");
@@ -42,7 +79,7 @@ public class CustomerMenu {
             }
             else{
                 System.out.println("Some internal error occurred, Please try again ");
-                displayRegisterMenu();
+                customerRegisterForm();
             }
 
         }catch (CustomerExistException e){
@@ -52,6 +89,47 @@ public class CustomerMenu {
 
 
         }
+    }
+    public void customerLoginForm(){
+
+    }
+
+    public void customerUpdateForm(){
+        try{
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please update entering the following details\n");
+            System.out.println("Enter Your Id : ");
+            String id = scanner.nextLine();
+            System.out.println("Enter Name : ");
+            String name = scanner.nextLine();
+            System.out.println("Enter E-Mail : ");
+            String email = scanner.nextLine();
+            System.out.println("Enter Password : ");
+            String password = scanner.nextLine();
+            Customer customer = new Customer();
+            customer.setId(id)
+                    .setName(name)
+                    .setEmail(email)
+                    .setPassword(password);
+            Customer updateCustomer = Factory.customerController.updateCustomer(customer);
+            System.out.println("Customer Updated Successfully.");
+//            displaypalyCustomerDetails(updateCustomer);
+        } catch (Exception e) {
+            System.out.println("Please");
+        }
+
+    }
+
+
+    public void displayMenuHeader(String menuHeader){
+        printDashLines();
+        String spaces = new String(new char[70]).replace('\0',' ');
+        System.out.printf("%-70s %-10s %-70s \n", spaces, menuHeader, spaces);
+        printDashLines();
+    }
+    public void printDashLines(){
+        String dashLines = new String(new char[150]).replace('\0','-');
+        System.out.println(dashLines);
     }
 }
 
