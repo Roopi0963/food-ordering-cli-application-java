d2993c4a22f03616fc40a8c71be3bf414a0e06fb
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-02-03 18:49:24 +0530

    feat: Implemented Restaurant CRUD operations

--- src/com/foodcliapp/service/RestaurantService.java
+++ src/com/foodcliapp/service/RestaurantService.java
@@ -1,9 +1,21 @@
 package com.foodcliapp.service;
 
 import com.foodcliapp.exceptions.RestaurantAlreadyExistsException;
+import com.foodcliapp.exceptions.RestaurantNotFoundException;
 import com.foodcliapp.model.Restaurant;
+
+import java.util.List;
 
 public interface RestaurantService {
     public Restaurant save(Restaurant restaurant) throws RestaurantAlreadyExistsException;
+
+    public Restaurant getRestaurantById(String id) throws RestaurantNotFoundException;
+
+    public List<Restaurant> getAllRestaurants();
+
+    public Restaurant updateRestaurant(Restaurant restaurant) throws RestaurantNotFoundException;
+
+    public void deleteRestaurant(String id) throws RestaurantNotFoundException;
+
 
 }
