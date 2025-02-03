a7da66a79ff29216fc9f5865ff6e4a97f940dcc7
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-02-03 18:49:13 +0530

    feat: Implemented Restaurant CRUD operations

--- src/com/foodcliapp/service/RestaurantServiceImpl.java
+++ src/com/foodcliapp/service/RestaurantServiceImpl.java
@@ -1,17 +1,20 @@
 package com.foodcliapp.service;
 
 import com.foodcliapp.exceptions.RestaurantAlreadyExistsException;
+import com.foodcliapp.exceptions.RestaurantNotFoundException;
 import com.foodcliapp.model.Restaurant;
 import com.foodcliapp.repository.RestaurantRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class RestaurantServiceImpl implements RestaurantService{
 
-    private RestaurantRepository restaurantRepository;
+    private final RestaurantRepository restaurantRepository;
 
 
     public RestaurantServiceImpl(RestaurantRepository restaurantRepository) {
+        this.restaurantRepository = restaurantRepository;
     }
 
     @Override
@@ -21,6 +24,30 @@ public class RestaurantServiceImpl implements RestaurantService{
         if(restaurantById.isPresent())
             throw new RestaurantAlreadyExistsException("Restaurant is already exists : "+restaurant.getId());
         return this.restaurantRepository.save(restaurant);
+
+    }
+
+    @Override
+    public Restaurant getRestaurantById(String id) throws RestaurantNotFoundException {
+        Optional<Restaurant> restaurantById = this.restaurantRepository.findRestaurantById(id);
+        if (restaurantById.isEmpty())
+            throw new RestaurantNotFoundException("Restaurant is not found with id : "+id);
+        return restaurantById.get();
+    }
+
+    @Override
+    public List<Restaurant> getAllRestaurants() {
+
+        return this.restaurantRepository.getRestaurantList();
+    }
+
+    @Override
+    public Restaurant updateRestaurant(Restaurant restaurant) throws RestaurantNotFoundException {
+        return null;
+    }
+
+    @Override
+    public void deleteRestaurant(String id) throws RestaurantNotFoundException {
 
     }
 }
