0111d6d1a33975f60026d3e3a5aa80abce659b80
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-02-05 23:13:26 +0530

    Refactor: Refactored the CRUD operations

--- src/com/foodcliapp/controller/RestaurantController.java
+++ src/com/foodcliapp/controller/RestaurantController.java
@@ -1,6 +1,11 @@
 package com.foodcliapp.controller;
 
+import com.foodcliapp.exceptions.RestaurantAlreadyExistsException;
+import com.foodcliapp.exceptions.RestaurantNotFoundException;
+import com.foodcliapp.model.Restaurant;
 import com.foodcliapp.service.RestaurantServiceImpl;
+
+import java.util.List;
 
 public class RestaurantController {
     private RestaurantServiceImpl restaurantService;
@@ -8,4 +13,27 @@ public class RestaurantController {
     public RestaurantController(RestaurantServiceImpl restaurantService) {
         this.restaurantService = restaurantService;
     }
+
+    public Restaurant save(Restaurant restaurant) throws RestaurantAlreadyExistsException{
+        return this.restaurantService.save(restaurant);
+    }
+
+    public List<Restaurant> getAllRestaurants(){
+        return this.restaurantService.getAllRestaurants();
+    }
+
+    public Restaurant getRestaurantById(String id) throws RestaurantNotFoundException {
+        return this.restaurantService.getRestaurantById(id);
+
+    }
+
+    public Restaurant updateRestaurant(Restaurant restaurant) throws RestaurantNotFoundException{
+        return this.restaurantService.updateRestaurant(restaurant);
+    }
+
+    public void deleteRestaurant(String id) throws RestaurantNotFoundException{
+        this.restaurantService.deleteRestaurant(id);
+
+    }
+
 }
--- src/com/foodcliapp/service/RestaurantServiceImpl.java
+++ src/com/foodcliapp/service/RestaurantServiceImpl.java
@@ -8,6 +8,7 @@ import com.foodcliapp.model.Restaurant;
 import com.foodcliapp.repository.DishRepository;
 import com.foodcliapp.repository.RestaurantRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -48,17 +49,36 @@ public class RestaurantServiceImpl implements RestaurantService{
 
     @Override
     public Restaurant updateRestaurant(Restaurant restaurant) throws RestaurantNotFoundException {
-        return null;
+        Optional<Restaurant> restaurantById = this.restaurantRepository.findRestaurantById(restaurant.getId());
+        if (restaurantById.isEmpty())
+            throw new RestaurantNotFoundException("Restaurant not found with id : " +restaurant.getId());
+        return this.restaurantRepository.updateRestaurant(restaurant);
     }
 
     @Override
     public void deleteRestaurant(String id) throws RestaurantNotFoundException {
+        Optional<Restaurant> restaurantById = this.restaurantRepository.findRestaurantById(id);
+        if (restaurantById.isEmpty())
+            throw new RestaurantNotFoundException("Restaurant not found with id : "+id);
+        this.restaurantRepository.deleteRestaurant(restaurantById.get());
 
     }
 
     @Override
     public List<Dish> getDishItems(String id) throws RestaurantNotFoundException, DishNotFoundException {
-        return List.of();
+        Optional<Restaurant> restaurantById = this.restaurantRepository.findRestaurantById(id);
+        if (restaurantById.isEmpty())
+            throw new RestaurantNotFoundException("Restaurant not found with id " + id);
+        List<Dish> dishList = new ArrayList<>();
+        Restaurant restaurant = restaurantById.get();
+        List<String> dishIds = restaurant.getMenu();
+//        for (String dishId : dishIds){
+//            Dish dish = dishRepository;
+//            dishList.add(dish);
+//        }
+
+        return dishList;
+            
     }
 
 
