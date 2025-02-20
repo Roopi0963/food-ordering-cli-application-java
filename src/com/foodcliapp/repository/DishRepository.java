b30a528cafe65f5f721032a8899527fc59830996
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-02-20 23:39:07 +0530

    feat : Implemented Dish CRUD operation

--- src/com/foodcliapp/repository/DishRepository.java
+++ src/com/foodcliapp/repository/DishRepository.java
@@ -29,6 +29,22 @@ public class DishRepository {
         return this.dishList.stream().filter(dish -> dish.getId().equals(id)).findFirst();
     }
 
-    
+    public void deleteDish(Dish dish){
+        this.dishList.remove(dish);
+    }
+
+    public Dish updateDish(Dish dishToBeUpdated){
+        Optional<Dish> updateDish = this.dishList.stream().filter(dish -> dish.getId().equals(dishToBeUpdated))
+                .findFirst()
+                .map(dish -> {
+                    dish.setName(dishToBeUpdated.getName())
+                            .setDescription(dishToBeUpdated.getDescription())
+                            .setPrice(dishToBeUpdated.getPrice());
+                    return dish;
+                });
+        return updateDish.orElse(null);
+    }
+
+
 
 }
