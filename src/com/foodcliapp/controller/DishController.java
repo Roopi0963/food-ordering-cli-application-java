04e05955a1de6d506d2caaa860aaefe8d491c637
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-02-20 23:39:32 +0530

    feat : Implemented Dish CRUD operation

--- src/com/foodcliapp/controller/DishController.java
+++ src/com/foodcliapp/controller/DishController.java
@@ -1,4 +1,17 @@
 package com.foodcliapp.controller;
 
+import com.foodcliapp.exceptions.DishExistException;
+import com.foodcliapp.model.Dish;
+import com.foodcliapp.repository.DishRepository;
+
 public class DishController {
+    private DishRepository dishRepository;
+
+    public DishController(DishRepository dishRepository) {
+        this.dishRepository = dishRepository;
+    }
+
+    public Dish save(Dish dish) throws DishExistException{
+        return this.dishRepository.save(dish);
+    }
 }
