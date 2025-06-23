018040b8517a0f9fbc8b4cb20d37697c8d5ef6e8
Roopi0963 <palegariroopeshnaidu@gmail.com>
2025-06-23 09:27:10 +0530

    update : updated main menu

---
+++ src/com/foodcliapp/Main.java
@@ -0,0 +1,18 @@
+package com.foodcliapp;
+
+import com.foodcliapp.exceptions.CustomerExistException;
+import com.foodcliapp.repository.CustomerRepository;
+import com.foodcliapp.ui.Menu;
+
+public class Main {
+    public static void main(String[] args) throws CustomerExistException {
+    Menu menu = new Menu();
+    menu.displayMenu();
+
+
+
+
+
+
+    }
+}
