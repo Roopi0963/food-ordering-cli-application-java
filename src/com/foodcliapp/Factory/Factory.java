75975507cce18125f4afbc61525f4409edce9f4b
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-06-25 22:49:11 +0530

    feat: Add Dish to Factory to generate dish objects

--- src/com/foodcliapp/Factory/Factory.java
+++ src/com/foodcliapp/Factory/Factory.java
@@ -1,15 +1,22 @@
 package com.foodcliapp.Factory;
 
 import com.foodcliapp.controller.CustomerController;
+import com.foodcliapp.controller.DishController;
 import com.foodcliapp.controller.RestaurantController;
 import com.foodcliapp.repository.CustomerRepository;
+import com.foodcliapp.repository.DishRepository;
 import com.foodcliapp.repository.RestaurantRepository;
 import com.foodcliapp.service.CustomerServiceImpl;
+import com.foodcliapp.service.DishServiceImpl;
 import com.foodcliapp.service.RestaurantServiceImpl;
 
 public class Factory {
 
     public static CustomerController customerController;
+
+    public static RestaurantController restaurantController;
+
+    public static DishController dishController;
 
     public static CustomerRepository getCustomerRepository(){
         return new CustomerRepository();
@@ -36,4 +43,16 @@ public class Factory {
     public static RestaurantController getrestaurantController(){
         return new RestaurantController(getRestaurantService());
     }
+
+    public static DishRepository getDishRepository(){
+        return new DishRepository();
+    }
+    public static DishServiceImpl getDishService(){
+        return new DishServiceImpl(getDishRepository());
+    }
+    public static DishController getDishController(){
+        return new DishController(getDishService());
+    }
+
+
 }
