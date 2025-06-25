8d0de107c9a0e3c7e538fb07861a942735fd355b
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-06-25 22:46:29 +0530

    update : updated dish methods

--- src/com/foodcliapp/controller/DishController.java
+++ src/com/foodcliapp/controller/DishController.java
@@ -3,38 +3,37 @@ package com.foodcliapp.controller;
 import com.foodcliapp.exceptions.DishExistException;
 import com.foodcliapp.exceptions.DishNotFoundException;
 import com.foodcliapp.model.Dish;
-import com.foodcliapp.repository.DishRepository;
 import com.foodcliapp.service.DishServiceImpl;
 
-import java.awt.*;
 import java.util.List;
 
 public class DishController {
-    private final DishServiceImpl dishServiceImp;
+    private final DishServiceImpl dishService;
 
 
-    public DishController(DishRepository dishRepository) {
-        this.dishServiceImp = new DishServiceImpl(dishRepository);
+    public DishController(DishServiceImpl dishService) {
+
+        this.dishService = dishService;
     }
 
     public Dish save(Dish dish) throws DishExistException{
-        return this.dishServiceImp.save(dish);
+        return this.dishService.save(dish);
     }
 
     public List<Dish> getDisesList(){
-        return this.dishServiceImp.dishList();
+        return this.dishService.dishList();
     }
 
     public Dish getDishById(String id) throws DishNotFoundException {
-        return this.dishServiceImp.findDishById(id);
+        return this.dishService.findDishById(id);
     }
 
     public Dish updateDish(Dish dish) throws DishNotFoundException {
-        return this.dishServiceImp.updateDish(dish);
+        return this.dishService.updateDish(dish);
     }
 
     public void deleteDish(String id) throws DishNotFoundException {
-        this.dishServiceImp.deleteDish(id);
+        this.dishService.deleteDish(id);
     }
 
 
