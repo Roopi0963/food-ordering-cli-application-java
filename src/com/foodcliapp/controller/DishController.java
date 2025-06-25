7065c3b4b652df17fc16dda7a0fc78f577d1e019
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-06-25 21:27:37 +0530

    update : updated dish methods

--- src/com/foodcliapp/controller/DishController.java
+++ src/com/foodcliapp/controller/DishController.java
@@ -6,6 +6,9 @@ import com.foodcliapp.model.Dish;
 import com.foodcliapp.repository.DishRepository;
 import com.foodcliapp.service.DishServiceImpl;
 
+import java.awt.*;
+import java.util.List;
+
 public class DishController {
     private final DishServiceImpl dishServiceImp;
 
@@ -16,6 +19,14 @@ public class DishController {
 
     public Dish save(Dish dish) throws DishExistException{
         return this.dishServiceImp.save(dish);
+    }
+
+    public List<Dish> getDisesList(){
+        return this.dishServiceImp.dishList();
+    }
+
+    public Dish getDishById(String id) throws DishNotFoundException {
+        return this.dishServiceImp.findDishById(id);
     }
 
     public Dish updateDish(Dish dish) throws DishNotFoundException {
